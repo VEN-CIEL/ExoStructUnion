ddd0640e57c6b124c7d4d297dd443bb7bee97bdf
ESTEBAN VENY <estebanveny@gmail.com>
2024-11-26 09:33:33 +0100

    Commit #2

--- Transgerbeur.cpp
+++ Transgerbeur.cpp
@@ -2,8 +2,43 @@
 
 using namespace std;
 
+
+
+union UContenu;
+
+struct SPalette
+{
+	int poids;
+	char reference[20];
+};
+
+struct SRouleau
+{
+	float longueur;
+	int numero;
+};
+
+enum EEtat { vide, palette, rouleau };
+
+struct SCasier
+{
+	EEtat etat;
+	int px, py;
+	union
+	{
+		SPalette paletteCasier;
+		SRouleau rouleauCasier;
+	};
+};
+
+//struct SCasier[];
+
 int main()
 {
 	cout << "Transgerbeur";
 
+	int px, py, poids, numero;
+	char reference;
+	float longueur;
+	
 }
